15241c0151ce6040272305c7ebecb3f756999dcd
estefania <eabaroj@gmail.com>
2019-07-05 16:35:27 -0600

    Func extract_coords fixed

--- voroni_maps_public_libraries/VoronoiMap_PublicLibYYC.py
+++ voroni_maps_public_libraries/VoronoiMap_PublicLibYYC.py
@@ -14,81 +14,84 @@ def import_data(csv_file):
 def extract_coords(dframe):
 	""" Define location by latitude and longitude """
 	
-	loc = df["Location"].str.strip('()').str.split(',', expand=True).rename(columns={0:"Lat", 1:"Lon"})
-	lat = loc['Lat'].values.astype(float)
-	lon = loc['Lon'].values.astype(float)
+	loc = dframe["Location"].str.strip('()').str.split(',', expand=True).rename(columns={0:"Lat", 1:"Lon"})
+	latitude = loc['Lat'].values.astype(float)
+	longitude = loc['Lon'].values.astype(float)
 
-	return lat, lon
+	return latitude, longitude
 
 def voronoi_finite_polygons_2d(vor, radius=None):
 	""" Define new regions and new vertices """
 
-    if vor.points.shape[1] != 2:
-        raise ValueError("Requires 2D input")
+	if vor.points.shape[1] != 2:
+		raise ValueError("Requires 2D input")
 
-    new_regions = []
-    new_vertices = vor.vertices.tolist()
+	new_regions = []
+	new_vertices = vor.vertices.tolist()
 
-    center = vor.points.mean(axis=0)
-    if radius is None:
-        radius = vor.points.ptp().max()
+	center = vor.points.mean(axis=0)
+	if radius is None:
+		radius = vor.points.ptp().max()
 
-    # Construct a map containing all ridges for a given point
-    all_ridges = {}
-    for (p1, p2), (v1, v2) in zip(vor.ridge_points, vor.ridge_vertices):
-        all_ridges.setdefault(p1, []).append((p2, v1, v2))
-        all_ridges.setdefault(p2, []).append((p1, v1, v2))
+	# Construct a map containing all ridges for a given point
+	all_ridges = {}
+	for (p1, p2), (v1, v2) in zip(vor.ridge_points, vor.ridge_vertices):
+		all_ridges.setdefault(p1, []).append((p2, v1, v2))
+		all_ridges.setdefault(p2, []).append((p1, v1, v2))
 
-    # Reconstruct infinite regions
-    for p1, region in enumerate(vor.point_region):
-        vertices = vor.regions[region]
+	# Reconstruct infinite regions
+	for p1, region in enumerate(vor.point_region):
+		vertices = vor.regions[region]
 
-        if all(v >= 0 for v in vertices):
-            # finite region
-            new_regions.append(vertices)
-            continue
+		if all(v >= 0 for v in vertices):
+		# finite region
+			new_regions.append(vertices)
+			continue
 
-        # reconstruct a non-finite region
-        ridges = all_ridges[p1]
-        new_region = [v for v in vertices if v >= 0]
+		# reconstruct a non-finite region
+		ridges = all_ridges[p1]
+		new_region = [v for v in vertices if v >= 0]
 
-        for p2, v1, v2 in ridges:
-            if v2 < 0:
-                v1, v2 = v2, v1
-            if v1 >= 0:
-                # finite ridge: already in the region
-                continue
+		for p2, v1, v2 in ridges:
+			if v2 < 0:
+				v1, v2 = v2, v1
 
-            # Compute the missing endpoint of an infinite ridge
+			if v1 >= 0:
+				# finite ridge: already in the region
+				continue
 
-            t = vor.points[p2] - vor.points[p1] # tangent
-            t /= np.linalg.norm(t)
-            n = np.array([-t[1], t[0]])  # normal
+			# Compute the missing endpoint of an infinite ridge
 
-            midpoint = vor.points[[p1, p2]].mean(axis=0)
-            direction = np.sign(np.dot(midpoint - center, n)) * n
-            far_point = vor.vertices[v2] + direction * radius
+			t = vor.points[p2] - vor.points[p1] # tangent
+			t /= np.linalg.norm(t)
+			n = np.array([-t[1], t[0]])  # normal
 
-            new_region.append(len(new_vertices))
-            new_vertices.append(far_point.tolist())
+			midpoint = vor.points[[p1, p2]].mean(axis=0)
+			direction = np.sign(np.dot(midpoint - center, n)) * n
+			far_point = vor.vertices[v2] + direction * radius
 
-        # sort region counterclockwise
-        vs = np.asarray([new_vertices[v] for v in new_region])
-        c = vs.mean(axis=0)
-        angles = np.arctan2(vs[:,1] - c[1], vs[:,0] - c[0])
-        new_region = np.array(new_region)[np.argsort(angles)]
+			new_region.append(len(new_vertices))
+			new_vertices.append(far_point.tolist())
 
-        # finish
-        new_regions.append(new_region.tolist())
+		# sort region counterclockwise
+		vs = np.asarray([new_vertices[v] for v in new_region])
+		c = vs.mean(axis=0)
+		angles = np.arctan2(vs[:,1] - c[1], vs[:,0] - c[0])
+		new_region = np.array(new_region)[np.argsort(angles)]
 
-    return new_regions, np.asarray(new_vertices)
+		# finish
+		new_regions.append(new_region.tolist())
 
+	return new_regions, np.asarray(new_vertices)
 
 
 def main ():
 	
 	input_file = 'Calgary_Public_Library_Locations_and_Hours.csv'
 	df = import_data(input_file)
+
+	lat, loc = extract_coords(df)
+	
 
 	return 0
 
